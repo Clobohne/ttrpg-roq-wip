cf26058d406ab9c95d94474cc1d96ec5d644207c
HimmelreichMarcel <62555695+HimmelreichMarcel@users.noreply.github.com>
2024-12-24 14:43:02 +0100

    add process

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 import SettingsProvider from './context/SettingsContext';
+import process from 'process';
+window.process = process;
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
